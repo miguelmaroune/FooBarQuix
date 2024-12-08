d057ac83d74f123358f03c0d71d393daa006b12b
miguelmaroune <miguelmaroune@gmail.com>
2024-12-08 11:43:22 +0100

    Initial commit

---
+++ src/main/java/com/foo/bar/batch/data_batch_processor/DataBatchProcessorApplication.java
@@ -0,0 +1,13 @@
+package com.foo.bar.batch.data_batch_processor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataBatchProcessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataBatchProcessorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/foo/bar/batch/data_batch_processor/DataBatchProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foo.bar.batch.data_batch_processor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataBatchProcessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
